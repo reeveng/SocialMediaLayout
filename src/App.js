091936179f991fbf75181b548d421a845c7cb937
reeveng <govaertreeven@gmail.com>
2020-11-01 22:53:10 +0100

    setup project routing, scss and homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
+import { Route, Switch } from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <PublicRoute exact path="/" component={Home} heroActive />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+
+const PublicRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={matchProps => (
+        <Component {...matchProps} />
+      )}
+    />
+  );
+};
